9e322529f70be2591efd9040b03b8b4823fcefaa
Nwosu Joseph Chuks <chuksdj07@gmail.com>
2022-05-24 20:18:35 +0800

    created navbar

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const  icon =  document.getElementById("icon")
+const navList = document.getElementById("navList")
+
+icon.addEventListener("click", () => {
+   navList.classList.toggle("hidden")
+
+
+} )
